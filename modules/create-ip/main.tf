73a3f0cd6baba132e3b11c3a223b2136fef13638
esf25t3 <luis.mendez@atos.net>
2021-10-16 12:59:59 -0500

    creation of lab 01

---
+++ modules/create-ip/main.tf
@@ -0,0 +1,33 @@
+data "google_compute_network" "vpc_network" {
+  project                 = var.project_id
+  name                    = var.vpc_name  
+}
+
+data "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = var.subnet_name    
+  network       = data.google_compute_network.vpc_network.id
+}
+
+
+
+/******************************************
+ Creating Static Internal IP 
+ *****************************************/
+resource "google_compute_address" "static_internal_address" {   
+  name         = var.name_internal_ip
+  subnetwork   = data.google_compute_subnetwork.default.id
+  address_type = "INTERNAL"
+  #address      = var.static_internal_ip
+  project      = var.project_id
+  region       = var.region
+}
+
+
+
+output "instance_ip_addr" {
+  value       = google_compute_address.static_internal_address.address
+  
+}
+
+
+
---
+++ modules/create-ip/variables.tf
@@ -0,0 +1,18 @@
+
+variable "project_id" {
+  type=string
+}
+variable "vpc_name" {
+  type=string
+}
+variable "region" {
+  type=string
+}
+variable "subnet_name" {
+  type=string
+}
+
+
+variable "name_internal_ip" {
+  type=string
+}
