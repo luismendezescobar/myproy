ea885467d56992d5efe98fe519eb5d4cd9761e88
esf25t3 <luis.mendez@atos.net>
2021-10-15 19:38:49 -0500

    creating virtual machines 02

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -60,7 +60,7 @@ resource "google_compute_firewall" "allow-health-check-to-wsfc-nodes" {
   allow {
     protocol = "tcp"
   }
-  source_ranges=[]"130.211.0.0/22","35.191.0.0/16"]
+  source_ranges=["130.211.0.0/22","35.191.0.0/16"]
 
   target_tags = ["wsfc-node"]
 }
