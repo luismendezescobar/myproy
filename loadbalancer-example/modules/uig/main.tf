cbd12a1984abed429c941294c9da193b22cabc8b
esf25t3 <luis.mendez@atos.net>
2021-11-30 12:08:39 -0600

    load balancing creation 12

--- loadbalancer-example/modules/uig/main.tf
+++ loadbalancer-example/modules/uig/main.tf
@@ -8,6 +8,11 @@ locals {
   all_ports = var.frontend_ports != [] ? concat(var.frontend_ports, local.ports) : local.ports
 }
 
+data "google_compute_instance" "instance_to_balancer" {
+   name         = "node-1"
+}
+
+
 
 
 resource "google_compute_instance_group" "webservers_zone" {
@@ -17,7 +22,7 @@ resource "google_compute_instance_group" "webservers_zone" {
   description = "Connect Ship Instance group for zone: ${each.value}"
 
   //instances = [for instance in var.instances : instance.self_link if instance.zone == each.value]
-  instances = [data.google_compute_instance.node-1.id]
+  instances = [data.google_compute_instance.instance_to_balancer.id]
 
   dynamic "named_port" {
     for_each = var.named_port
