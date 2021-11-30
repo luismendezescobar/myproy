202287b5f75d6928764955d0cc61ea0622dffdfc
esf25t3 <luis.mendez@atos.net>
2021-11-30 12:11:14 -0600

    load balancing creation 13

--- loadbalancer-example/modules/uig/main.tf
+++ loadbalancer-example/modules/uig/main.tf
@@ -9,11 +9,9 @@ locals {
 }
 
 data "google_compute_instance" "instance_to_balancer" {
-   name         = "node-1"
+   name     = "node-1"
+   zone     = "us-east1-b"
 }
-
-
-
 
 resource "google_compute_instance_group" "webservers_zone" {
   for_each = local.distinct_zones
