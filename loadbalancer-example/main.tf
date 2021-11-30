12076287a9e8c23340fa879fdc29e2e2d955af05
esf25t3 <luis.mendez@atos.net>
2021-11-30 08:34:21 -0600

    load balancing creation 04

--- loadbalancer-example/main.tf
+++ loadbalancer-example/main.tf
@@ -67,7 +67,7 @@ module "unmanaged_instance_group" {
 
   //for_each      = local.loadbalancer_map
   //name          = join("-", ["unmanaged-instance", lower(each.key)]) 
-  name          = join("-", ["unmanaged-instance", local.loadbalancer_map[0]) 
+  name          = join("-", ["unmanaged-instance", "group") 
   project_id    = var.project_id
   region        = var.region 
   network       = local.network_self_link
