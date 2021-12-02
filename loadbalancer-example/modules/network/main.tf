bd9b2b85b48d0d6f3dac3918ecc405af1058026b
esf25t3 <luis.mendez@atos.net>
2021-12-02 17:23:00 -0600

    internal load balancer 12

--- loadbalancer-example/modules/network/main.tf
+++ loadbalancer-example/modules/network/main.tf
@@ -62,6 +62,6 @@ resource "google_compute_firewall" "allow-health-check-to-wsfc-nodes" {
   }
   source_ranges=["130.211.0.0/22","35.191.0.0/16","209.85.204.0/22","209.85.152.0/22"]
 
-  target_tags = ["fw-gcp-hc-all"]
+
 }
 
