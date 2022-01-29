9db001d548202c856f18a85f8a567b6f9acc8d00
esf25t3 <luis.mendez@atos.net>
2022-01-29 17:51:08 -0600

    alwayson test 25

--- SQL_Alwayson/modules/create-load-balancer/main.tf
+++ SQL_Alwayson/modules/create-load-balancer/main.tf
@@ -65,7 +65,7 @@ resource "azurerm_lb_rule" "sql" {
   frontend_port                   = "0"
   backend_port                    = "0"
   frontend_ip_configuration_name  = "SQLFrontEnd"
-  backend_address_pool_id         = azurerm_lb_backend_address_pool.lbbap.id
+  #backend_address_pool_id         = azurerm_lb_backend_address_pool.lbbap.id
   probe_id                        = azurerm_lb_probe.sql-probe.id  
 }
 
