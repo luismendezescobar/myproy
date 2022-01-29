2b154948ac5979561058bbdb30753c90a82a7864
esf25t3 <luis.mendez@atos.net>
2022-01-29 17:48:17 -0600

    alwayson test 24

--- SQL_Alwayson/modules/create-load-balancer/main.tf
+++ SQL_Alwayson/modules/create-load-balancer/main.tf
@@ -52,7 +52,7 @@ resource "azurerm_lb_rule" "ntc" {
   frontend_port                   = "0"
   backend_port                    = "0"
   frontend_ip_configuration_name  = "ClusterFrontEnd"
-  backend_address_pool_id         = azurerm_lb_backend_address_pool.lbbap.id
+  #backend_address_pool_id         = azurerm_lb_backend_address_pool.lbbap.id
   probe_id                        = azurerm_lb_probe.ntc-probe.id  
 }
 
