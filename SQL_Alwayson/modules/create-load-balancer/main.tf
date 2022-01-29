34e4eba3c64d0f51f22101e487eb0eaa1a4e87b2
esf25t3 <luis.mendez@atos.net>
2022-01-29 16:24:48 -0600

    alwayson test 08

--- SQL_Alwayson/modules/create-load-balancer/main.tf
+++ SQL_Alwayson/modules/create-load-balancer/main.tf
@@ -77,17 +77,18 @@ resource "azurerm_availability_set" "avset" {
 }
 
 ##############Here comes the tricky part####################################################
+/*
 locals {
   server_nics={
     for key in var.instances : key.testout.name =>key.testout.id
   }
 
 }
-
+*/
 
 resource "azurerm_network_interface_backend_address_pool_association" "nicbapassoc" {
-  for_each                = local.server_nics
-  #network_interface_id    = azurerm_network_interface.nic[count.index].id
+  #for_each                = local.server_nics
+  for_each                = var.instances
   network_interface_id    = each.value.id
   ip_configuration_name   = "ipconfig1"
   backend_address_pool_id = azurerm_lb_backend_address_pool.lbbap.id
