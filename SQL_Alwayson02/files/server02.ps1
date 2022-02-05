2cd2d981fa86ccac7566ffdc5d96eef6e5ed0ee5
esf25t3 <luis.mendez@atos.net>
2022-02-05 11:51:32 -0600

    alwayson 252022-04

--- SQL_Alwayson02/files/server02.ps1
+++ SQL_Alwayson02/files/server02.ps1
@@ -469,11 +469,11 @@ New-Partition -DiskNumber 2 -UseMaximumSize -DriveLetter F
 Format-Volume -DriveLetter F -FileSystem NTFS -NewFileSystemLabel sql-binaries
 
 Start-Sleep -Seconds 5
+Install-WindowsFeature Failover-Clustering –IncludeManagementTools
 
 
 New-NetFirewallRule -DisplayName 'SQL-Always-on' -Profile @('Domain', 'Private','Public') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('1433', '5022','59998','59999')
 
-Install-WindowsFeature Failover-Clustering –IncludeManagementTools
 
 
 
