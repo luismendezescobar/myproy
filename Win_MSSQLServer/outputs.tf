0d9e8c770dddd8efb9b7654340a75a16065b17d9
esf25t3 <luis.mendez@atos.net>
2022-02-10 08:49:41 -0600

    always on test 02

--- Win_MSSQLServer/outputs.tf
+++ Win_MSSQLServer/outputs.tf
@@ -1,3 +1,4 @@
+/*
 output "nodes" {
   description = "Map output of the Node names and their IP addresses"
   value = zipmap(azurerm_virtual_machine.vm[*].name, azurerm_network_interface.nic[*].private_ip_address)
@@ -7,3 +8,4 @@ output "adminpassword" {
   description = "Admin login account password"
   value = var.local_vm_password
 }
+*/
