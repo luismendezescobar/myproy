6b369229b5b940bb9bc87388dfb6a0eb7ce84db3
esf25t3 <luis.mendez@atos.net>
2022-03-21 16:56:21 -0600

    exercises 3-21-2021-02

---
+++ powershell/Untitled1.ps1
@@ -0,0 +1,67 @@
+﻿
+$file = 'c:\temp\test\test.txt'
+#If the file does not exist, create it.
+
+if (-not(Test-Path -Path $file -PathType Leaf)) {
+     try {
+         $null = New-Item -ItemType File -Path $file -Force -ErrorAction Stop
+         Set-Content $file 1
+         Write-Host "The file [$file] has been created."
+     }
+     catch {
+         throw $_.Exception.Message
+     }
+}
+else {
+    $file_content=get-Content $file
+    if($file_content -eq 1){        #in this part we assume that the server is already in the domain
+    	Write-Host "the content of the file is:$file_content"
+        $res=[int]$file_content+1
+        Set-Content $file $res
+		###first check if the nodes are already in the domain
+		$d = @("node-1","node-2")
+		$a_result=@()
+        $cont=0
+		for (;;)
+		{
+			Foreach ($server in $d)
+			{   Try {
+					Get-ADComputer $server -ErrorAction Stop
+					$a_result+="true"					
+				}
+				Catch {
+					$a_result+="false"
+				}				
+			}				
+			Foreach($element in $a_result)
+			{
+				if($element -eq "false"){			
+					$Result="false"
+					$a_result=@()
+					break
+				}
+				else{
+					$Result="true"
+				}				
+			}
+
+			if($Result -eq "true"){
+				break               #we may continue to the next step
+			}
+            else{
+                $cont++
+            }
+            if ($cont -eq 10){
+                Write-Host "Too many efforts with no success"
+                break
+            }
+
+		}
+		Write-Host "do something here in the server"
+		
+    }
+	else{
+		Write-Host "the script has been completed"
+		exit 0
+	}
+}
--- s2d/modules/create-vm-windows/witness-node.ps1
+++ s2d/modules/create-vm-windows/witness-node.ps1
@@ -4,14 +4,12 @@ if ($op.Count -eq 0) {
    net user /add localvmadmin Passw0rd12345! 
    net localgroup administrators localvmadmin /add
 } 
-#else {
-#	Write-Host "User exist localvmadmin exiting"
-#	exit 0
-#}
 ###########################################################################################
 #after we add the user we indicate that we have completed the first step
-$file = 'c:\temp\test\test.txt'
+
+#$file = 'c:\temp\test\test.txt'
 #If the file does not exist, create it.
+<#
 if (-not(Test-Path -Path $file -PathType Leaf)) {
      try {
          $null = New-Item -ItemType File -Path $file -Force -ErrorAction Stop
@@ -22,27 +20,121 @@ if (-not(Test-Path -Path $file -PathType Leaf)) {
          throw $_.Exception.Message
      }
 }
- else {
-     $file_content=get-Content $file
-     if($file_content -eq 1){        #in this part we assume that the server is already in the domain
-        Write-Host "the content of the file is:$file_content"
+else {
+    $file_content=get-Content $file
+    if($file_content -eq 1){        #in this part we assume that the server is already in the domain
+    	Write-Host "the content of the file is:$file_content"
         $res=[int]$file_content+1
         Set-Content $file $res
+		###first check if the nodes are already in the domain
+		$d = @("node-1","node-2")
+		$a_result=@()
+		for (;;)
+		{
+			Foreach ($server in $d)
+			{   Try {
+					Get-ADComputer $server -ErrorAction Stop
+					$a_result+="true"					
+				}
+				Catch {
+					$a_result+="false"
+				}				
+			}				
+			Foreach($element in $a_result)
+			{
+				if($element -eq "false"){			
+					$Result="false"
+					$a_result=@()
+					break
+				}
+				else{
+					$Result="true"
+				}				
+			}
 
+			if($Result -eq "true"){
+				break               #we may continue to the next step
+			}
+		}
+		
 		New-Item "C:\QWitness" –type directory
 		New-SmbShare `
   		-Name QWitness `
   		-Path "C:\QWitness" `
   		-Description "SQL File Share Witness" `
   		-FullAccess  $env:username,node-1$,node-2$,example\Administrator
-     }
-	 else{
+    }
+	else{
 		Write-Host "the script has been completed"
-	 }
- }
+		exit 0
+	}
+}
+#>
 ##########################################################################################
+<#
+$res_domain=(Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain
+
+if ($res_domain){
+	Try {
+		Get-ADComputer "node-1" -ErrorAction Stop		
+		$node1_found=$true
+	}
+	Catch {
+		$node1_found=$false
+	}		
+
+	Try {
+		Get-ADComputer "node-2" -ErrorAction Stop		
+		$node2_found=$true
+	}
+	Catch {
+		$node2_found=$false
+	}		
+	if ($node1_found -and $node2_found){
+		if(Test-Path -Path "C:\QWitness" -eq $false){	
+			New-Item "C:\QWitness" –type directory
+			New-SmbShare `
+			-Name QWitness `
+			-Path "C:\QWitness" `
+			-Description "SQL File Share Witness" `
+			-FullAccess  $env:username,node-1$,node-2$,example\Administrator
+		}	
+	}
+	exit 0
+}
+#>
+
+
+$res_domain=(Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain
+
+if ($res_domain){
+	Write-Output "The server is already in domain"
+	Try {
+		Get-ADComputer "node-1" -ErrorAction Stop		
+		$node1_found=$true
+	}
+	Catch {
+		$node1_found=$false
+	}
+	Try {
+		Get-ADComputer "node-2" -ErrorAction Stop		
+		$node2_found=$true
+	}
+	Catch {
+		$node2_found=$false
+	}			
+	Write-Output "node-1 result is: $node1_found"
+	Write-Output "node-1 result is: $node2_found"
+	if ($node1_found -and $node2_found){
+		Write-Output "both nodes were found"
+	} 
+
+
+	exit 0
+}
 
 add-windowsfeature FS-FileServer
+Add-WindowsFeature RSAT-AD-PowerShell
 
 $DNSPrimary = "10.0.0.12"
 netsh interface ip set dns Ethernet static $DNSPrimary
