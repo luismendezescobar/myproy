c1403543c40ea6eaff84ef00b0a8d9aa30401a46
esf25t3 <luis.mendez@atos.net>
2022-04-17 21:46:44 -0500

    shellscripts-4-17-22-08

--- shell-scripts/varexample.sh
+++ shell-scripts/varexample.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+
+
+MYUSERNAME="username"
+MYPASSWORD="password123"
+STARTOFSCRIPT=`date`
+
 echo "The current path is: $PATH"
+
+echo "My login name is:$MYUSERNAME"
+echo "My login password is: $MYPASSWORD"
+echo "I started this script at: $STARTOFSCRIPT"
+
+ENDOFSCRIPT=`date`
+
+echo "I ended this script at:$ENDOFSCRIPT"
