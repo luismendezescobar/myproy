3824232911db9363e18e4e1e62569c8e141c480f
esf25t3 <luis.mendez@atos.net>
2022-04-17 19:38:41 -0500

    shellscripts-4-17-22-04

---
+++ shell-scripts/env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+
+echo "This script will give us environment information"
+echo "================================================"
+echo ""
+echo "Hello Username:$USER"
+echo ""
+echo "Your Home Directory is: $LOGIN"
