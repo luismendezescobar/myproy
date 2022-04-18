a465251ddc183e490ba50cdef6e2dfff29570b2c
esf25t3 <luis.mendez@atos.net>
2022-04-17 21:55:48 -0500

    shellscripts-4-17-22-09

---
+++ shell-scripts/comments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#this is a comment
+# and anything that it does
+
+clear         #clears the screen
+
+MYUSERNAME="Luis"  #user name for this application
+
+echo "We are using the user:$MYUSERNAME"  #Display to the console
+
+DATETIMESTAMP=`date`
+
+echo "date when the script was ran:$DATETIMESTAMP"
