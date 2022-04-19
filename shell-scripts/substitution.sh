4eed581fe0fb9a01c4b4ce78e41e0ad95f6831dd
esf25t3 <luis.mendez@atos.net>
2022-04-18 19:41:34 -0500

    Scripts added today 4-18-2022-01

---
+++ shell-scripts/substitution.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#This script is intende dto show how to do simple substitution
+
+TODAYSDATE=`date`
+USERFILES=`find /home -user user`
+
+echo "Today's Date: $TODAYSDATE"
+echo "All files Owned by USER: $USERFILES"
