0285a87b1d6f0620af0f2e53e6a41f8857f2845a
esf25t3 <luis.mendez@atos.net>
2022-04-18 19:42:48 -0500

    Scripts added today 4-18-2022-02

--- shell-scripts/substitution.sh
+++ shell-scripts/substitution.sh
@@ -2,7 +2,7 @@
 #This script is intende dto show how to do simple substitution
 
 TODAYSDATE=`date`
-USERFILES=`find /home -user user`
+USERFILES=`find /home -user cloud_user`
 
 echo "Today's Date: $TODAYSDATE"
 echo "All files Owned by USER: $USERFILES"
