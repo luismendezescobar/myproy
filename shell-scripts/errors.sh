cfce0a7333950221865f7dc2d56976c1fe3ea109
luis.mendez <luismendeze@hotmail.com>
2022-04-25 22:11:33 -0500

    changes 4-25-2022-07

--- shell-scripts/errors.sh
+++ shell-scripts/errors.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #this script is to show exit status types
+set -e   #with this option we are telling the shell that the moment
+         #that it receives an exit error stop the program right there
 
 expr 1 + 5
 echo $?
