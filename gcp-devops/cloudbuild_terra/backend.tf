ce4e3dc08ba3e26e3afd665980dc8670855d777e
luis.mendez <luismendeze@hotmail.com>
2022-05-19 10:24:41 -0500

    devops-5-19-2022-02

---
+++ gcp-devops/cloudbuild_terra/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "gcs" {
+    bucket  = "mybucket5-19-2022"
+    prefix  = "state"
+  }  
+  required_version = ">= 0.12.7"  
+  required_providers { 
+    google = {
+      source = "hashicorp/google"
+      version = "3.82.0"
+    }
+  }
+}
+provider "google" {
+  project = "triggering-a-198-cfefe4f5"
+  #region  = "<regione_name>"
+  #zone    = "<zone_name>"
+}
---
+++ gcp-devops/cloudbuild_terra/var-file.tfvars
@@ -0,0 +1,2 @@
+project_id       = "triggering-a-198-cfefe4f5"
+terraform_bucket = "mybucket5-19-2022"
