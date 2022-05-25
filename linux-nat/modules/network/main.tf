1abcbcebe41dc3a9e9266d06b05402d044f779a1
luis.mendez <luismendeze@hotmail.com>
2022-05-24 21:20:13 -0500

    linux nat 5-24-2022-06

--- linux-nat/modules/network/main.tf
+++ linux-nat/modules/network/main.tf
@@ -22,7 +22,7 @@ resource "google_compute_firewall" "allow_ssh" {
     protocol = "tcp"
     ports    = ["22"]
   }
-  source_ranges="0.0.0.0/0"
+  source_ranges=["0.0.0.0/0"]
 }
 /*
 resource "google_compute_firewall" "allow-all-between-wsfc-nodes" {
