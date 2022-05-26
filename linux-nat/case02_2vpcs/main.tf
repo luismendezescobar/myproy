5050084987f76311697b9cf03e48409752717e1f
luis.mendez <luismendeze@hotmail.com>
2022-05-25 22:14:19 -0500

    linux nat 5-25-2022-16

--- linux-nat/case02_2vpcs/main.tf
+++ linux-nat/case02_2vpcs/main.tf
@@ -40,7 +40,7 @@ module "vm_instances_creation" {
   can_ip_forward            = each.value.can_ip_forward
 
   depends_on = [
-    module.network
+    module.vpc_creation
   ]
 
 }
@@ -68,7 +68,7 @@ module "vm_double_nic" {
   can_ip_forward            = each.value.can_ip_forward
 
   depends_on = [
-    module.network
+    module.vpc_creation
   ]
 
 }
