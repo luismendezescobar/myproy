4ae10b244edf172931df1f419d6172167c6a66c7
luis.mendez <luismendeze@hotmail.com>
2022-05-25 22:06:39 -0500

    linux nat 5-25-2022-14

---
+++ linux-nat/case01_access_internet/main.tf
@@ -0,0 +1,40 @@
+module "network" {
+  for_each = var.vpc_info
+  source = "./modules/network"
+  project_id    = var.project_id
+  vpc_name      = each.key
+  subnetworks   = each.value.subnetworks  
+ 
+
+}
+
+//we need to create another rule to allow the comunication in the network (allow all)
+
+module "vm_instances_creation" {
+  for_each                  = var.server_vm_info
+  source                    = "./modules/create-vm"  
+  server_name               = each.key
+
+  project_id                = var.project_id
+  #region                    = var.region
+  zone                      = each.value.zone
+  
+  instance_description      = each.value.description
+  network_tags             = each.value.network_tags
+  instance_machine_type     = each.value.instance_type
+  source_image              = each.value.source_image
+  subnetwork_project        = var.project_id
+  subnetwork                = each.value.subnet_name
+  init_script               = each.value.init_script
+  auto_delete               = each.value.auto_delete
+  disk_size_gb              = each.value.boot_disk_size_gb
+  boot_disk_type            = each.value.boot_disk_type
+  additional_disks          = each.value.additional_disks
+  external_ip               = each.value.external_ip 
+  can_ip_forward            = each.value.can_ip_forward
+
+  depends_on = [
+    module.network
+  ]
+
+}
---
+++ linux-nat/case02_2vpcs/modules/firewall-rules/main.tf
@@ -0,0 +1,76 @@
+resource "google_compute_firewall" "vpc_shared_ssh_allow" {
+  project = var.project_id
+  name    = join("-",["vpc-shared","ssh-allow"])
+  network = "vpc-shared"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges=["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "vpc_local_ssh_allow" {
+  project = var.project_id
+  name    = join("-",["vpc-local","ssh-allow"])
+  network = "vpc-local"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges=["0.0.0.0/0"]
+}
+/*******************************************************************/
+
+resource "google_compute_firewall" "vpc_shared_allow_all" {
+  project = var.project_id
+  name    = join("-",["vpc-shared","allow-all"])
+  network = "vpc-shared"
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+  } 
+  allow {
+    protocol = "udp"
+  }  
+  source_ranges=["10.10.10.0/0"]
+}
+resource "google_compute_firewall" "vpc_local_allow_all" {
+  project = var.project_id
+  name    = join("-",["vpc-local","allow-all"])
+  network = "vpc-local"
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+  } 
+  allow {
+    protocol = "udp"
+  }  
+  source_ranges=["10.10.11.0/0"]
+}
+/****************************************************************************/
+
+resource "google_compute_firewall" "vpc_shared_http_allow" {
+  project = var.project_id
+  name    = join("-",["vpc-shared","http-allow"])
+  network = "vpc-shared"
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges=["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "vpc_local_http_allow" {
+  project = var.project_id
+  name    = join("-",["vpc-local","http-allow"])
+  network = "vpc-local"
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges=["0.0.0.0/0"]
+}
