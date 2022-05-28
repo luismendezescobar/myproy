848eaac8ff7f12158bed42bd9450d133d311cfd7
luis.mendez <luismendeze@hotmail.com>
2022-05-28 10:49:09 -0500

    linux nat 5-28-2022-02

--- linux-nat/case03_2vpcs_lb/modules/create-vm/nat_init.sh
+++ linux-nat/case03_2vpcs_lb/modules/create-vm/nat_init.sh
@@ -1,2 +1,5 @@
-sysctl -w net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo sysctl -w net.ipv4.ip_forward=1
+#iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo yum install httpd -yum
+echo "Example web page to pass health check" | tee /var/www/html/index.html
+sudo systemctl enable --now httpd
