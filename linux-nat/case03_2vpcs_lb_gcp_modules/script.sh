a6208c7786179cb1a28ec174d236e54037a3bede
luis.mendez <luismendeze@hotmail.com>
2022-06-04 21:05:45 -0500

    linux-nat 6-4-2022-03

---
+++ linux-nat/case03_2vpcs_lb_gcp_modules/script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "replacing version in file .terraform/modules/vpc_creation/modules/vpc/versions.tf"
+echo "from 0.126 to ~> 1.1.0"  
+sed -i 's/required_version = \"~> 0.12.6\"/required_version = \"~> 1.1.0\"/' .terraform/modules/vpc_creation/modules/vpc/versions.tf
