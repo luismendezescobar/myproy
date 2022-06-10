d9288791db11c2c3516692ba9cc58115b67e93f8
luis.mendez <luismendeze@hotmail.com>
2022-06-09 22:25:07 -0500

    linux-nat 6-9-2022-08

--- linux-nat/case03_2vpcs_lb_gcp_modules02/modules/nat_init.sh
+++ linux-nat/case03_2vpcs_lb_gcp_modules02/modules/nat_init.sh
@@ -2,6 +2,11 @@
 # Enable IP forwarding:
 sudo sysctl -w net.ipv4.ip_forward=1
 echo "net.ipv4.ip_forward=1" > /etc/sysctl.d/20-iptables.conf
+
+#this one is needed for centos 7 otherwise won't work
+echo "net.ipv4.ip_forward=1" > /etc/sysctl.conf
+
+
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 #iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 # Read VM network configuration:
