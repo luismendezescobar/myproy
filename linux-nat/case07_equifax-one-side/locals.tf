75352d174a59300f4fa92c8a017ba9251662b1c7
luis.mendez <luismendeze@hotmail.com>
2022-06-21 20:09:31 -0500

    linux-nat 6-21-2022-07

---
+++ linux-nat/case07_equifax-one-side/locals.tf
@@ -0,0 +1,12 @@
+/*
+
+locals {
+  local_account= {
+    email= var.compute_account
+    scopes= ["cloud-platform"]
+  }
+  
+  var.instance_template_map.nat-server.service_account.email= "649715744425-compute@developer.gserviceaccount.com"
+}
+
+*/
