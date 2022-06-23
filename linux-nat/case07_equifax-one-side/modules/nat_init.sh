bad7c47d9833c413fd30ac7f35136497942138f1
luis.mendez <luismendeze@hotmail.com>
2022-06-23 16:29:06 -0500

    linux-nat 6-23-2022-05

--- linux-nat/case07_equifax-one-side/modules/nat_init.sh
+++ linux-nat/case07_equifax-one-side/modules/nat_init.sh
@@ -46,6 +46,11 @@ sudo ip rule add pri 32000 from $nic1_gw/$nic1_mask table rt-nic1
 sleep 1
 sudo ip route add 35.191.0.0/16 via $nic1_gw dev $nic1_id table rt-nic1
 sudo ip route add 130.211.0.0/22 via $nic1_gw dev $nic1_id table rt-nic1
+sudo ip route add 172.16.1.0/24 via $nic1_gw dev $nic1_id table rt-nic1
+sudo ip route add 192.168.1.0/24 via $nic1_gw dev $nic1_id table rt-nic1
+
+
+
 # Use a web server to pass the health check for this example.
 #sudo yum install httpd -y
 #sudo echo "Example web page to pass health check" | tee /var/www/html/index.html
@@ -53,9 +58,10 @@ sudo ip route add 130.211.0.0/22 via $nic1_gw dev $nic1_id table rt-nic1
 #this configuration works fine
 
 #this one is for the instance to respond on the secondary nic
+#this broke down the previous routing so I had to use the one above
 #sudo ifconfig eth1 10.10.10.4 netmask 255.255.255.0 broadcast 10.10.10.4 mtu 1430
-echo "1 rt1" | sudo tee -a /etc/iproute2/rt_tables
-sudo ip route add $nic1_gw src $nic1_addr dev $nic1_id table rt1
-sudo ip route add default via $nic1_gw dev $nic1_id table rt1
-sudo ip rule add from $nic1_addr/$nic1_mask table rt1
-sudo ip rule add to $nic1_addr/$nic1_mask  table rt1
+#echo "1 rt1" | sudo tee -a /etc/iproute2/rt_tables
+#sudo ip route add $nic1_gw src $nic1_addr dev $nic1_id table rt1
+#sudo ip route add default via $nic1_gw dev $nic1_id table rt1
+#sudo ip rule add from $nic1_addr/$nic1_mask table rt1
+#sudo ip rule add to $nic1_addr/$nic1_mask  table rt1
