af40113ad706a42513b3dd7d213ededcf9cead18
luis.mendez <luismendeze@hotmail.com>
2022-06-23 14:54:27 -0500

    linux-nat 6-23-2022-04

--- linux-nat/case07_equifax-one-side/modules/nat_init.sh
+++ linux-nat/case07_equifax-one-side/modules/nat_init.sh
@@ -9,6 +9,7 @@ yum install iptables-services -y
 sudo systemctl enable --now iptables
 
 
+sudo iptables -F     #for testing
 sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 #sudo iptables -A FORWARD -p icmp -j DROP
 #sudo iptables -A FORWARD -p tcp --dport 80 -j ACCEPT
@@ -17,8 +18,11 @@ sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 #sudo iptables -A FORWARD -p tcp --dport 1:1024 -j DROP
 #if we add ports above 1024 is going to act up
 
-
-
+#the correct configuraton would be, remove the iptables -F then add the below rules
+#sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+#sudo iptables -I FORWARD -p tcp --dport 80 -m state --state NEW -j ACCEPT
+#sudo iptables -I FORWARD -p tcp --dport 443 -m state --state NEW -j ACCEPT
+#sudo iptables -I FORWARD -p tcp -m state --state related,ESTABLISH -j ACCEPT
 
 
 
@@ -47,3 +51,11 @@ sudo ip route add 130.211.0.0/22 via $nic1_gw dev $nic1_id table rt-nic1
 #sudo echo "Example web page to pass health check" | tee /var/www/html/index.html
 #sudo systemctl enable --now httpd
 #this configuration works fine
+
+#this one is for the instance to respond on the secondary nic
+#sudo ifconfig eth1 10.10.10.4 netmask 255.255.255.0 broadcast 10.10.10.4 mtu 1430
+echo "1 rt1" | sudo tee -a /etc/iproute2/rt_tables
+sudo ip route add $nic1_gw src $nic1_addr dev $nic1_id table rt1
+sudo ip route add default via $nic1_gw dev $nic1_id table rt1
+sudo ip rule add from $nic1_addr/$nic1_mask table rt1
+sudo ip rule add to $nic1_addr/$nic1_mask  table rt1
