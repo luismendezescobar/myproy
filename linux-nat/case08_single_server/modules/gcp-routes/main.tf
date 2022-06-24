d3c4d6745ba2fde497fa6552544ce701d524b4f1
luis.mendez <luismendeze@hotmail.com>
2022-06-24 12:31:47 -0500

    linux-nat 6-24-2022-myproy-08

--- linux-nat/case08_single_server/modules/gcp-routes/main.tf
+++ linux-nat/case08_single_server/modules/gcp-routes/main.tf
@@ -4,7 +4,7 @@ resource "google_compute_route" "from_local_to_outside" {
   dest_range            = "0.0.0.0/24"
   network               = "vpc-local"
   next_hop_instance     = "nat-server"
-  next_hop_instance_zone="us-central-b"
+  next_hop_instance_zone="us-central1-b"
   priority              = 900
   tags                  = ["no-ip"]
 }
