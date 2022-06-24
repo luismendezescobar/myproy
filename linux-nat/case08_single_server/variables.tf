a37fdd4c3df0f11ec135746edc29687f0ad0b041
luis.mendez <luismendeze@hotmail.com>
2022-06-24 12:24:47 -0500

    linux-nat 6-24-2022-myproy-05

--- linux-nat/case08_single_server/variables.tf
+++ linux-nat/case08_single_server/variables.tf
@@ -67,6 +67,31 @@ variable "cloud_nat_map" {
   }))
 }
 
+variable "server_vm_info_two_nics" {
+  description = "the number of DB server instances"
+  type = map(object({
+    zone              = string
+    instance_type     = string
+    source_image      = string
+    boot_disk_size_gb = number    
+    boot_disk_type    = string
+    network_tags     = list(string)    
+    description       = string
+    init_script       = string
+    auto_delete       = bool
+    subnet_name1       = string
+    subnet_name2       = string
+    external_ip       = list(string)
+    can_ip_forward    = bool
+    additional_disks = list(object({
+      name         = string
+      disk_size_gb = number
+      disk_type    = string
+    }))
+  }))
+  default = {}
+}
+
 variable "server_vm_info" {
   description = "the number of DB server instances"
   type = map(object({
