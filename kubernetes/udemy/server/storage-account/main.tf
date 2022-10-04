c416eb3332ae59ee23110d2ec4cbebdfd5e39980
luis.mendez <luismendeze@hotmail.com>
2022-10-04 18:47:58 -0500

    changes for 10-4-2022

--- kubernetes/udemy/server/storage-account/main.tf
+++ kubernetes/udemy/server/storage-account/main.tf
@@ -12,7 +12,7 @@ resource "random_string" "random" {
 
 
 resource "azurerm_storage_account" "my_storage_account" {
-  name                     = lower("mystorage-${resource.random_string.random.result}")
+  name                     = lower("mystorage${resource.random_string.random.result}")
   resource_group_name      = data.azurerm_resource_group.my_resource_group.name
   location                 = data.azurerm_resource_group.my_resource_group.location
   account_tier             = "Standard"
