fa013c1576ac37001e27ca89c32bb96518b6c574
luis.mendez <luismendeze@hotmail.com>
2022-10-04 20:01:25 -0500

    changes for 10-4-2022

--- kubernetes/udemy/server/backend.tf
+++ kubernetes/udemy/server/backend.tf
@@ -18,4 +18,5 @@ provider "azurerm" {
 	subscription_id  = var.subguid
     #tenant_id       = "da67ef1b-ca59-4db2-9a8c-aa8d94617a16"      #manually here
     features {}
+    skip_provider_registration = "true"
 }
