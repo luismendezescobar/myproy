f613845d195d833e4742d169fb3fe84ca618fb16
luis.mendez <luismendeze@hotmail.com>
2022-11-01 22:38:49 -0600

    changes for today

--- kubernetes/istio/05-observing-network/install-ingress-gw-kiali.sh
+++ kubernetes/istio/05-observing-network/install-ingress-gw-kiali.sh
@@ -6,3 +6,14 @@ kubectl label namespace $GATEWAY_NS \
 istio.io/rev=$REVISION --overwrite
 kubectl apply -n $GATEWAY_NS \
   -f ./istio-ingressgateway
+
+#download the correct version of istio to install the tools
+#https://istio.io/latest/docs/setup/getting-started/#download
+#table of versions
+#https://kiali.io/docs/installation/installation-guide/prerequisites/
+#install kiali
+#https://kiali.io/docs/installation/quick-start/
+#upload an executable
+#git update-index --chmod=+x install-ingress-gw-kiali.sh
+
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.12 TARGET_ARCH=x86_64 sh -
