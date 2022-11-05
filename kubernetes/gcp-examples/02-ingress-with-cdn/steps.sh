b5b0218a7db3ae20c8ba14a6a4f7e4bd30614c2a
luis.mendez <luismendeze@hotmail.com>
2022-11-04 19:43:53 -0600

    changes for today

--- kubernetes/gcp-examples/02-ingress-with-cdn/steps.sh
+++ kubernetes/gcp-examples/02-ingress-with-cdn/steps.sh
@@ -1,2 +1,25 @@
 #follow this one
-https://cloud.google.com/kubernetes-engine/docs/how-to/ingress-features#ingress
+#https://cloud.google.com/kubernetes-engine/docs/how-to/ingress-features#ingress
+
+
+#follow also with the examples from here
+#https://cloud.google.com/kubernetes-engine/docs/concepts/ingress-xlb
+#https://cloud.google.com/kubernetes-engine/docs/how-to/ingress-http2
+#https://cloud.google.com/service-mesh/docs/by-example/canary-deployment
+
+
+#To use a BackendConfig to configure Cloud CDN, perform the following tasks:
+
+#CDN example
+
+kubectl create namespace cdn-how-to
+kubectl apply -f 01-deployment.yaml
+kubectl apply -f 02-backendconfig.yaml
+kubectl apply -f 03-service.yaml
+
+#Create a reserved IP address:
+gcloud compute addresses create cdn-how-to-address --global
+
+kubectl apply -f 04-ingress.yaml
+
+kubectl describe ingress my-ingress --namespace=cdn-how-to | grep "Address"
