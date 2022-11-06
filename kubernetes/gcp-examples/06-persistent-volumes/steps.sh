16fff40d7a652e6c6e708808ff4c371ef43de8f0
luis.mendez <luismendeze@hotmail.com>
2022-11-06 15:16:22 -0600

    changes for today

--- kubernetes/gcp-examples/06-persistent-volumes/steps.sh
+++ kubernetes/gcp-examples/06-persistent-volumes/steps.sh
@@ -5,3 +5,8 @@
 #https://cloud.google.com/kubernetes-engine/docs/concepts/multi-cluster-ingress
 #https://cloud.google.com/kubernetes-engine/docs/how-to/multi-cluster-ingress-setup#before_you_begin
 #https://cloud.google.com/kubernetes-engine/docs/how-to/multi-cluster-ingress
+
+kubectl create secret generic mysql \
+    --from-literal=password="Passw0rd!"
+
+gcloud compute addresses create ingress-ip --global
