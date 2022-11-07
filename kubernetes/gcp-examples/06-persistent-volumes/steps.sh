4896639716e892355ba1930d2f4a749f75ebfe8a
luis.mendez <luismendeze@hotmail.com>
2022-11-06 21:53:25 -0600

    changes for today

--- kubernetes/gcp-examples/06-persistent-volumes/steps.sh
+++ kubernetes/gcp-examples/06-persistent-volumes/steps.sh
@@ -6,7 +6,22 @@
 #https://cloud.google.com/kubernetes-engine/docs/how-to/multi-cluster-ingress-setup#before_you_begin
 #https://cloud.google.com/kubernetes-engine/docs/how-to/multi-cluster-ingress
 
+#install the deployment gateway from here:
+cd ~
+git clone https://github.com/GoogleCloudPlatform/anthos-service-mesh-packages
+cd anthos-service-mesh-packages
+#create a namespace and label it for istio pod injection
+kubectl create ns asm-ingress
+REVISION=$(kubectl get deploy -n istio-system -l app=istiod -o \
+jsonpath={.items[*].metadata.labels.'istio\.io\/rev'}'{"\n"}')
+echo $REVISION
+kubectl label namespace asm-ingress istio-injection- istio.io/rev=$REVISION --overwrite
+
+k apply -f samples/gateways/istio-ingressgateway/ -n asm-ingress
+
+
 kubectl create secret generic mysql \
     --from-literal=password="Passw0rd!"
 
 gcloud compute addresses create ingress-ip --global
+
