ba4d2c2fcab55d8a54a188b6d71d05105294c8a4
luis.mendez <luismendeze@hotmail.com>
2022-11-24 12:14:56 -0600

    changes for today

---
+++ app-lovin/iam_permissions/03-iam.tf
@@ -0,0 +1,18 @@
+module "organization-iam-bindings" {
+  source        = "terraform-google-modules/iam/google//modules/organizations_iam"
+  #organizations = ["my-organization_one", "my-organization_two"]
+  mode          = "authoritative"
+
+  bindings = {
+    "roles/analyticshub.viewer" = [
+      "serviceAccount:sa-for-myproject@my-project-91055-366623.iam.gserviceaccount.com",
+      "group:gcp-devops-group@luismendeze.com",
+      "user:test@luismendeze.com",
+    ]
+    "roles/appengine.appViewer" = [
+      "serviceAccount:my-service-account@prod-project-369617.iam.gserviceaccount.com",
+      "group:gcp-devops-group@luismendeze.com",
+      "user:devops-user01@luismendeze.com",
+    ]
+  }
+}
