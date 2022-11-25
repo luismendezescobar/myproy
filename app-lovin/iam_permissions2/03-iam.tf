a6b1ca01fed26835122a73ed331311e8a66a3a17
luis.mendez <luismendeze@hotmail.com>
2022-11-25 17:59:34 -0600

    Changes for today

--- app-lovin/iam_permissions2/03-iam.tf
+++ app-lovin/iam_permissions2/03-iam.tf
@@ -2,23 +2,12 @@ locals {
     json_files = fileset("${path.module}/files-iam","*.*")  
     json_data= { for file_name in local.json_files :
                 replace(file_name, ".json", "")=>jsondecode(file("${path.module}/files-iam/${file_name}"))} 
-/*    
+    
     maps= flatten([for value in local.json_data:value.map_to_sa] )         
     all_maps={
       for x in local.maps:join("-",[x.iam_project_id,x.sa,x.role]) => x
     }
-*/
-    map_sa1={for key, value in local.json_data: key =>value.map_to_sa}
-    map_sa2 = flatten([ for key, value in local.map_sa1:[
-                            for item in value:{
-                                project_id=key
-                                principal=item.principal
-                                role=item.role
-                                sa=item.sa
-                            }
-                          ]
-                      ])
-    map_sa3 = {for x in local.map_sa2:join("-",[x.project_id,x.sa,x.role]) => x}
+
 }
 
 
@@ -41,7 +30,7 @@ resource "google_service_account_iam_binding" "admin-account-iam" {
   members = each.value.principal
 }
 
-/*
+
 output "bindings" {
   value= {for key,value in local.json_data:key=>value} 
 }
@@ -53,4 +42,3 @@ output "map_output" {
 output "sa_map" {
   value=local.all_maps
 }
-*/
