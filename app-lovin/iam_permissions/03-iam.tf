887c03a4cc90352aab3500a6b06707102dedd9a8
luis.mendez <luismendeze@hotmail.com>
2022-11-24 23:09:43 -0600

    changes for today

--- app-lovin/iam_permissions/03-iam.tf
+++ app-lovin/iam_permissions/03-iam.tf
@@ -2,32 +2,15 @@ locals {
     json_files = fileset("${path.module}/files-iam","*.*")  
     json_data= { for file_name in local.json_files :
                 replace(file_name, ".json", "")=>jsondecode(file("${path.module}/files-iam/${file_name}"))} 
-}
+    
+    maps= flatten([for value in local.json_data:value.map_to_sa] )         
+    all_maps={
+      for x in local.maps:join("-",[x.iam_project_id,x.sa,x.role]) => x
+    }
 
+}
 
 /*
-module "organization-iam-bindings" {
-  source   = "terraform-google-modules/iam/google//modules/projects_iam"
-  version = "7.4.1"
-  for_each = local.json_data
-  projects = ["my-project-91055-366623"]
-  mode          = "additive"
-
-  bindings = {
-    "roles/analyticshub.viewer" = [
-      "serviceAccount:sa-for-myproject@my-project-91055-366623.iam.gserviceaccount.com",
-      #"group:gcp-devops-group@luismendeze.com",
-      "user:test@luismendeze.com",
-    ]
-    "roles/appengine.appViewer" = [
-      "serviceAccount:my-service-account@prod-project-369617.iam.gserviceaccount.com",
-      #"group:gcp-devops-group@luismendeze.com",
-      "user:devops-user01@luismendeze.com",
-    ]
-  }
-}
-*/
-
 module "project-iam-bindings" {
   source   = "terraform-google-modules/iam/google//modules/projects_iam"
   version = "7.4.1"
@@ -37,15 +20,14 @@ module "project-iam-bindings" {
 
   bindings = each.value.project_bindings
 }
-
+*/
 /*
 resource "google_service_account_iam_binding" "admin-account-iam" {
-  service_account_id = google_service_account.sa.name
-  role               = "roles/iam.serviceAccountUser"
+  for_each = local.json_data
+  service_account_id = "projects/${each.key}/ServiceAcounts/${each.value.map_to_sa}"
+  role               = each.value.map_to_sa.principal
 
-  members = [
-    "user:jane@example.com",
-  ]
+  members = each.value.map_to_sa.principal
 }
 */
 
@@ -55,4 +37,8 @@ output "bindings" {
 
 output "map_output" {
   value= {for key,value in local.json_data:key=>value.map_to_sa} 
+}
+
+output "sa_map" {
+  value=local.all_maps
 }
