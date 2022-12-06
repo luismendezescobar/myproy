4263a6e78652356b145e3ce93bc9f68aa5229087
luismendezescobar <luismendezescobar@gmail.com>
2022-12-06 12:58:44 -0600

    changes for today

---
+++ app-lovin/gsuite-group/01-provider.tf
@@ -0,0 +1,22 @@
+/*we used this article
+
+https://github.com/terraform-google-modules/terraform-google-project-factory/blob/master/modules/gsuite_enabled/README.md
+*/
+terraform {
+
+  required_providers { 
+    google = {
+      source = "hashicorp/google"
+      version = "~>4.43"       //this is the hashicorp modules version
+    }
+  }
+}
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+}
---
+++ app-lovin/gsuite-group/02-gsuite.tf
@@ -0,0 +1,5 @@
+resource "gsuite_group" "example" {
+  email       = "test_group@luismendeze.com"
+  name        = "test_group@luismendeze.com"
+  description = "Example group"
+}
