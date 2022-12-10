4f67ad5299f118e1b1a4387c18c7a4f321380331
luismendezescobar <luismendezescobar@gmail.com>
2022-12-09 18:57:55 -0600

    changes for today

--- app-lovin/project-factory-gsuite/01-provider.tf
+++ app-lovin/project-factory-gsuite/01-provider.tf
@@ -30,3 +30,10 @@ provider "google-beta" {
   region  = var.region
 }
 
+provider "gsuite" {  
+  impersonated_user_email = "luis@luismendeze.com"
+  oauth_scopes = [
+  "https://www.googleapis.com/auth/admin.directory.group",
+  "https://www.googleapis.com/auth/admin.directory.user"
+]
+}
