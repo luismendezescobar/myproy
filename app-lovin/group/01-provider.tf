ddfc847789bb442b0453396bbc9cc5ee4b7c9698
luismendezescobar <luismendezescobar@gmail.com>
2022-12-09 18:30:37 -0600

    changes for today

---
+++ app-lovin/group/01-provider.tf
@@ -0,0 +1,45 @@
+/*we used this article
+
+https://github.com/terraform-google-modules/terraform-google-project-factory/blob/master/modules/gsuite_enabled/README.md
+*/
+terraform {
+
+  required_providers { 
+    google = {
+      source = "hashicorp/google"
+      version = "~>4.43"       //this is the hashicorp modules version
+    }
+    gsuite = {
+      source = "DeviaVir/gsuite"
+      version = "0.1.62"
+    }
+  }
+}
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+}
+
+/*
+provider "gsuite" {  
+  oauth_scopes = [
+    "https://www.googleapis.com/auth/admin.directory.group",
+    "https://www.googleapis.com/auth/apps.groups.settings",
+    "https://www.googleapis.com/auth/admin.directory.user",
+    "https://www.googleapis.com/auth/admin.directory.userschema",
+  ]
+}
+*/
+provider "gsuite" {  
+  project = var.project_id
+  impersonated_user_email = "luis@luismendeze.com"
+  oauth_scopes = [
+  "https://www.googleapis.com/auth/admin.directory.group",
+  "https://www.googleapis.com/auth/admin.directory.user"
+]
+}
--- app-lovin/group/create_group.tf
+++ app-lovin/group/create_group.tf
@@ -5,13 +5,6 @@
 #-------------------------------------------------
 
 
-provider "gsuite" {  
-  impersonated_user_email = "luis@luismendeze.com"
-  oauth_scopes = [
-  "https://www.googleapis.com/auth/admin.directory.group",
-  "https://www.googleapis.com/auth/admin.directory.user"
-]
-}
 
 resource "gsuite_group" "skunkworks-team" {
   email       = "test-team@luismendeze.com"
