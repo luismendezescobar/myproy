eec12835f1cbb3aa4c735903d406ee5d40108ad4
luismendezescobar <luismendezescobar@gmail.com>
2022-12-11 18:44:37 -0600

    changes for today

--- app-lovin/project-factory/validate-group.sh
+++ app-lovin/project-factory/validate-group.sh
@@ -2,13 +2,18 @@ GROUP_NAME="gcp-new-lui@luismendeze.com"
 
 #extract the group name:
 
+for file in ./*.json; do
+    echo "$(basename "$file")"
+done
 
-RESULT_GROUP=$(gcloud identity groups search --labels="cloudidentity.googleapis.com/groups.discussion_forum" --organization=luismendeze.com |sed -n "/$GROUP_NAME/p")
 
-if [[ ${#RESULT_GROUP} -gt 0 ]]; then
-  echo "this is what I found $RESULT_GROUP"
-  GROUP_FOUND=1
-else
-  echo "I could not found the group called $GROUP_NAME"
-  GROUP_FOUND=0
-fi
+
+#RESULT_GROUP=$(gcloud identity groups search --labels="cloudidentity.googleapis.com/groups.discussion_forum" --organization=luismendeze.com |sed -n "/$GROUP_NAME/p")
+
+#if [[ ${#RESULT_GROUP} -gt 0 ]]; then
+#  echo "this is what I found $RESULT_GROUP"
+#  GROUP_FOUND=1
+#else
+#  echo "I could not found the group called $GROUP_NAME"
+#  GROUP_FOUND=0
+#fi
