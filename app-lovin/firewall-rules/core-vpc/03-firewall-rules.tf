4dbf1c488648dec023e3805bd92926c517558fe7
luismendezescobar <luismendezescobar@gmail.com>
2022-12-19 13:18:09 -0600

    changes for today

---
+++ app-lovin/firewall-rules/core-vpc/03-firewall-rules.tf
@@ -0,0 +1,15 @@
+
+module "project-factory" {
+  source = "terraform-google-modules/network/google//modules/firewall-rules"
+  version = "~> 6.0"  
+  
+  project_id   = var.project_id
+  network_name = var.network_name
+  rules        = var.rules
+  
+}
+
+
+/*
+GOOGLE_OAUTH_ACCESS_TOKEN="$(gcloud --impersonate-service-account=sa-vpc-landing-pipe@devops-369900.iam.gserviceaccount.com auth print-access-token)" terraform apply -var-file=./core-vpc/02-terraform.tfvars
+*/
---
+++ app-lovin/firewall-rules/variables.tf
@@ -0,0 +1,38 @@
+variable "project_id" {
+  type=string
+  default = "central-gcp-vpc-non-prod-37070"
+}
+
+variable "prod_core_network_name" {
+  type=string
+  default = "vpc-core-non-prod"
+}
+
+variable "rules" {
+  description = "List of custom rule definitions (refer to variables file for syntax)."
+  default     = []
+  type = list(object({
+    name                    = string
+    description             = string
+    direction               = string
+    priority                = number
+    ranges                  = list(string)
+    source_tags             = list(string)
+    source_service_accounts = list(string)
+    target_tags             = list(string)
+    target_service_accounts = list(string)
+    allow = list(object({
+      protocol = string
+      ports    = list(string)
+    }))
+    deny = list(object({
+      protocol = string
+      ports    = list(string)
+    }))
+    log_config = object({
+      metadata = string
+    })
+  }))
+}
+
+
