ca82606e51405c7a665d8dad87d6e8c289d40fb2
luismendezescobar <luismendezescobar@gmail.com>
2022-12-21 10:47:28 -0600

    changes for today

---
+++ python/people-fun/get-users.py
@@ -0,0 +1,29 @@
+# First, install the necessary libraries:
+#   pip install google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client
+
+# Import the necessary modules
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+
+# Set the project ID and service account email
+project_id = 'central-tech-gcp-resources'
+service_account_email = 'centraltech-gcp-iam-pipeline@central-tech-gcp-resources.iam.gserviceaccount.com'
+
+# Use the service account credentials to authenticate
+creds = Credentials.from_service_account_info({
+    'project_id': project_id,
+    'client_email': service_account_email,
+    'token_uri': 'https://oauth2.googleapis.com/token',
+    'private_key': 'my-private-key'
+})
+
+# Build the IAM service client
+iam_service = build('iam', 'v1', credentials=creds)
+
+# List all users in the project
+response = iam_service.projects().serviceAccounts().list(name='projects/' + project_id).execute()
+users = response['accounts']
+
+# Print the email addresses of all users
+for user in users:
+    print(user['email'])
