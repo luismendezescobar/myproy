32a45e88b43f96ec1cff18a2e85f8950a757a4b8
luismendezescobar <luismendezescobar@gmail.com>
2022-12-21 13:00:44 -0600

    changes for today

--- python/people-fun/get-users.py
+++ python/people-fun/get-users.py
@@ -1,29 +1,30 @@
-# First, install the necessary libraries:
-#   pip install google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client
 
-# Import the necessary modules
-from google.oauth2.credentials import credentials
-from googleapiclient.discovery import build
+#!/usr/bin/env python
+# Python script to reformat IAM json data
+# Rajathithan Rajasekar - 03/03/2020
+import json,ast,sys
 
-# Set the project ID and service account email
-project_id = 'central-tech-gcp-resources'
-service_account_email = 'centraltech-gcp-iam-pipeline@central-tech-gcp-resources.iam.gserviceaccount.com'
+data = []
 
-# Use the service account credentials to authenticate
-creds = credentials.from_authorized_user_info({
-    'project_id': project_id,
-    'client_email': service_account_email,
-    'token_uri': 'https://oauth2.googleapis.com/token',
-    'private_key': 'my-private-key'
-})
+project = ""
 
-# Build the IAM service client
-iam_service = build('iam', 'v1', credentials=creds)
+def hello(jdata, project):
+    nicedata = ast.literal_eval(json.dumps(jdata, indent=4, sort_keys=True))
+    for i in range(len(nicedata)):
+        indrow = nicedata[i]
+        usersandservice = indrow['members']
+        for j in range(len(usersandservice)):
+            print("{},{},{}".format(project, indrow['role'], usersandservice[j]))
 
-# List all users in the project
-response = iam_service.projects().serviceAccounts().list(name='projects/' + project_id).execute()
-users = response['accounts']
 
-# Print the email addresses of all users
-for user in users:
-    print(user['email'])
+project = sys.argv[1]
+data = sys.stdin.read()
+#data = data.encode("ascii", "replace")
+data = data.replace("u\'", "\"")
+data = data.replace("\'", "\"")
+# print(data)
+jdata = json.loads(data)
+hello(jdata, project)
+
+
+#GOOGLE_OAUTH_ACCESS_TOKEN="$(gcloud --impersonate-service-account=sa-vpc-landing-pipe@devops-369900.iam.gserviceaccount.com auth print-access-token)" python3 get-users.py central-tech-gcp-resources
