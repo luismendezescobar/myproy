f1c82599c3b9d68086647d1cd1eacf4f3b274a4e
luismendezescobar <luismendezescobar@gmail.com>
2023-01-11 18:12:22 -0600

    adding external account

---
+++ app-lovin/gsuite-group/group-settings/01-provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    googleworkspace = {
+      source = "hashicorp/googleworkspace"
+      version = "0.7.0"
+    }
+  }
+}
+/*
+provider "googleworkspace" {
+  # Configuration options
+}
+*/
