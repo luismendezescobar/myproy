af96cd0cc068a961ec824096938615ec8eaec0df
luismendezescobar <luismendezescobar@gmail.com>
2023-01-13 20:16:44 -0600

    new group code

--- app-lovin/group-workspace-test/03-create-group.tf
+++ app-lovin/group-workspace-test/03-create-group.tf
@@ -3,6 +3,6 @@ locals {
   map_owners={for x in var.owners:x=>"owners"}
   map_managers={for x in var.managers:x=>"managers"}
   map_members={for x in var.owners:x=>"members"}
-  map_all=merge(map_owners,map_managers,map_members)
+  map_all=merge(local.map_owners,local.map_managers,local.map_members)
 
 }
