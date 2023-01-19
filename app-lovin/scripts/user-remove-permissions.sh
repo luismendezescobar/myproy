1e6e527774a87f6b34f27e2085b525f0f53b40c6
luismendezescobar <luismendezescobar@gmail.com>
2023-01-18 21:45:15 -0600

    adding script

--- app-lovin/scripts/user-remove-permissions.sh
+++ app-lovin/scripts/user-remove-permissions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set the project ID
-project_id="pf-tnt-dev"
+project_id=$1
 
 # Get the user email from the command line
-user_email=$1
+user_email=$2
 
 # Get all roles assigned to the user in the project
 roles=($(gcloud projects get-iam-policy $project_id --filter="bindings.members:$user_email" --flatten="bindings[].members" --format="table(bindings.role)"|grep -v "^$" | cut -d ' ' -f2- ))
