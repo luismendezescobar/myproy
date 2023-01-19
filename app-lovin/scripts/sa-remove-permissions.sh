c95555a97a0dfd9e04c7925becf4eb1cd73a2201
luismendezescobar <luismendezescobar@gmail.com>
2023-01-18 21:40:08 -0600

    adding script

---
+++ app-lovin/scripts/sa-remove-permissions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the project ID
+project_id="pf-tnt-dev"
+
+# Get the user email from the command line
+user_email=$1
+
+# Get all roles assigned to the user in the project
+roles=($(gcloud projects get-iam-policy $project_id --filter="bindings.members:$user_email" --flatten="bindings[].members" --format="table(bindings.role)"|grep -v "^$" | cut -d ' ' -f2- ))
+echo "here are the roles ${roles[@]}"
+# Iterate through all roles and remove them for the user
+for role in "${roles[@]}"; do
+    gcloud projects remove-iam-policy-binding $project_id --member="serviceAccount:$user_email" --role=$role
+    echo "role $role has been removed."
+done
