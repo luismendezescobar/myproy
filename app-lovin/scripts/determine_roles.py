53ba354093cf2da7d0ac3da80d47bd8068dbfc94
luismendezescobar <luismendezescobar@gmail.com>
2023-01-25 14:16:05 -0600

    adding script

--- app-lovin/scripts/determine_roles.py
+++ app-lovin/scripts/determine_roles.py
@@ -1,19 +1,19 @@
 import google.auth
 from google.auth.transport.requests import Request
-from google.cloud import iam
+from google.cloud import iam_v1
 
 # Set the project ID
 project_id = "blocks-2"
 
 # Create a client for the IAM service
-client = iam.Client()
+client = iam_v1.IamClient()
 
 # Define the list of permissions
 permissions = ["appengine.services.list"]
 
 # Iterate through the permissions and display the roles that have them
 for permission in permissions:
-    roles = client.list_roles(permissions_filter=permission, parent=f"projects/{project_id}")
+    roles = client.list_roles(parent=f"projects/{project_id}", permission=permission)
     print(f"The role(s) that have the permission {permission} are:")
     for role in roles:
         print(role.name)
