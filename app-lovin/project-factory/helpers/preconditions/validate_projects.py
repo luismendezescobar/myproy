5d496a26586b454557e9b9ad392ac93b8cd199ae
luismendezescobar <luismendezescobar@gmail.com>
2023-02-03 14:54:40 -0600

    changes for today

--- app-lovin/project-factory/helpers/preconditions/validate_projects.py
+++ app-lovin/project-factory/helpers/preconditions/validate_projects.py
@@ -3,6 +3,7 @@
 import os
 import json
 import subprocess
+import sys
 
 
 directory = '../../files-projects'
@@ -15,11 +16,14 @@ for filename in os.listdir(directory):
             org_id = data.get('org_id')
             billing_account = data.get('billing_account')
             folder_id = data.get('folder_id')
+
             print(f"org_id: {org_id}, billing_account: {billing_account}, folder_id: {folder_id}")
 
             result = subprocess.run(['python', 'preconditions.py','--org_id',org_id,'--billing_account',billing_account,'--folder_id',folder_id], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             print(f"Exit code: {result.returncode}")
             if result.returncode != 0:
                 print("Child script failed.")
+                sys.exit(1)
             else:
                 print("Child script succeeded.")  
+                sys.exit(0)
