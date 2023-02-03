a6e46ea214363439993621a93d57aa6ac2a666bd
luismendezescobar <luismendezescobar@gmail.com>
2023-02-03 17:30:28 -0600

    changes for today

--- app-lovin/project-factory/helpers/preconditions/validate_projects.py
+++ app-lovin/project-factory/helpers/preconditions/validate_projects.py
@@ -6,11 +6,41 @@ import subprocess
 import sys
 
 
+def validate_keys_exists(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        org_id = data.get('org_id', None)
+        billing_account = data.get('billing_account', None)
+        folder_id = data.get('folder_id', None)
+        
+        if org_id and billing_account and folder_id:
+            print(f"org_id: {org_id}")
+            print(f"billing_account: {billing_account}")
+            print(f"folder_id: {folder_id}")
+            return 0
+        else:
+            #print("One or more keys were not found in the file.")
+            return 1
+
+
+
+
+
+
+
+
 directory = '../../files-projects'
 
 for filename in os.listdir(directory):
     if filename.endswith('.json'):
         file_path = os.path.join(directory, filename)
+        
+        if(validate_keys_exists(file_path)):
+            print(f"One or more keys were not found in the file.{filename}")
+            sys.exit(1)
+
+        
+        
         with open(file_path, 'r') as f:
             data = json.load(f)
             org_id = data.get('org_id')
@@ -35,3 +65,5 @@ for filename in os.listdir(directory):
             else:
                 print(f"Child script succeeded. Project:{filename}")  
                 #sys.exit(0)
+
+
