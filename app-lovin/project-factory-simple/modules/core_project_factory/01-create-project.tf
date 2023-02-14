d4e90ba52f72fd9979ec55980d1149b7f3a6684c
luismendezescobar <luismendezescobar@gmail.com>
2023-02-14 17:26:30 -0600

    changes for today

--- app-lovin/project-factory-simple/modules/core_project_factory/01-create-project.tf
+++ app-lovin/project-factory-simple/modules/core_project_factory/01-create-project.tf
@@ -1,9 +1,14 @@
+locals {   
+  project_org_id    = var.folder_id != "" ? null : var.org_id
+  project_folder_id = var.folder_id != "" ? var.folder_id : null  
+}
+
 resource "google_project" "main" {
   name                = var.name
   project_id          = var.project_id
-  org_id              = var.org_id
-  billing_account     = var.billing_account
-  folder_id           = var.folder_id  
+  org_id              = local.project_org_id
+  folder_id           = local.project_folder_id
+  billing_account     = var.billing_account  
   auto_create_network = var.auto_create_network
   labels              = var.labels
 }
