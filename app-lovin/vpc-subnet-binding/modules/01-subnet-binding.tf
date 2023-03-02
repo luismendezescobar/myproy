55811cdaa3d4d3d54a012f14d8a53ff721f49fa2
luismendezescobar <luismendezescobar@gmail.com>
2023-03-01 21:57:49 -0600

    adding network binding-07

--- app-lovin/vpc-subnet-binding/modules/01-subnet-binding.tf
+++ app-lovin/vpc-subnet-binding/modules/01-subnet-binding.tf
@@ -2,7 +2,7 @@
 https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork_iam
 */
 
-
+/*
 resource "google_compute_subnetwork_iam_binding" "binding" {
   project = var.project
   region = var.subnet_region
@@ -10,7 +10,7 @@ resource "google_compute_subnetwork_iam_binding" "binding" {
   role = "roles/compute.networkUser"
   members = var.principal
 }
-
+*/
 /*
 resource "google_compute_subnetwork_iam_member" "service_shared_vpc_subnet_users" {
   for_each = toset(var.principal)
@@ -19,6 +19,13 @@ resource "google_compute_subnetwork_iam_member" "service_shared_vpc_subnet_users
   subnetwork = var.subnet
   role = "roles/compute.networkUser"
   member = each.value
-
 }
 */
+
+resource "google_compute_subnetwork_iam_member" "service_shared_vpc_subnet_users" {  
+  project = var.project
+  region = var.subnet_region
+  subnetwork = var.subnet
+  role = "roles/compute.networkUser"
+  member = "serviceAccount:sa-test@new-luis-id.iam.gserviceaccount.com"
+}
