04f6d495a89b9653823716604b8e832cef8c9760
luismendezescobar <luismendezescobar@gmail.com>
2023-03-11 20:39:36 -0600

    pushing-10

--- python/flask/pets-adopt/pets-adopt.py
+++ python/flask/pets-adopt/pets-adopt.py
@@ -14,6 +14,20 @@ def index():
     <li><a href="/animals/rabbits">Rabbits</a></li>
   </ul>
   '''
+
+@app.route('/white')
+def index():
+  return f'''
+  <h1>Adopt a Pet!</h1>
+  <p>Browse through the links below to find your new furry friend:</p>
+  <ul>
+    <li><a href="/animals/dogs">Dogs</a></li>
+    <li><a href="/animals/cats">Cats</a></li>
+    <li><a href="/animals/rabbits">Rabbits</a></li>
+  </ul>
+  '''
+
+
 @app.route('/animals/<pet_type>')
 def animals(pet_type):
   html=f"<h1>List of {pet_type}</h1>"    
