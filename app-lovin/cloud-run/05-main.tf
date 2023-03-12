81d6b4349e0857100037060a634f7ef526c60191
luismendezescobar <luismendezescobar@gmail.com>
2023-03-12 17:50:58 -0600

    adding lb-04

--- app-lovin/cloud-run/05-main.tf
+++ app-lovin/cloud-run/05-main.tf
@@ -45,7 +45,7 @@ resource "google_compute_global_address" "external_ip" {
   ip_version   = "IPV4"
 }
 
-/*
+
 module "lb-http" {
   source            = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
   version           = "~> 7.0"
@@ -87,4 +87,3 @@ module "lb-http" {
     }
   }
 }
-*/
