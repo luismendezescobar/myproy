d549bd093442dd49be2ba70617f40461f58296f6
luismendezescobar <luismendezescobar@gmail.com>
2023-03-12 18:47:51 -0600

    adding lb-20

--- app-lovin/cloud-run/05-main.tf
+++ app-lovin/cloud-run/05-main.tf
@@ -38,6 +38,7 @@ resource "google_compute_region_network_endpoint_group" "default" {
     service = each.value.service_name
   }
 }
+/*
 module "lb-http" {
   source            = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
   version           = "~> 7.0"
@@ -85,3 +86,4 @@ module "lb-http" {
     }
   }
 }
+*/
