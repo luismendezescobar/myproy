d39cb122e0eb1c0cfec75fcb12cc9f538301803a
luismendezescobar <luismendezescobar@gmail.com>
2023-05-08 13:54:22 -0600

    adding locust-add TASK_SCRIPT env var

--- app-lovin/load-testing/locust-tasks/run.sh
+++ app-lovin/load-testing/locust-tasks/run.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 LOCUST="/usr/local/bin/locust"
-LOCUS_OPTS="-f /locust-tasks/tasks.py"
-LOCUST_MODE=${LOCUST_MODE:-standalone}
-
-if [[ "$LOCUST_MODE" = "master" ]]; then
-    LOCUS_OPTS="$LOCUS_OPTS --master"
-elif [[ "$LOCUST_MODE" = "worker" ]]; then
-    LOCUS_OPTS="$LOCUS_OPTS --slave --master-host=$LOCUST_MASTER"
-fi
+LOCUS_OPTS="-f /locust-tasks/$TASK_SCRIPT"
 
 echo "$LOCUST $LOCUS_OPTS"
 
