f2873fc74d3acaa058bf896a240f8b2801d1fdc1
luismendezescobar <luismendezescobar@gmail.com>
2023-10-16 21:28:51 -0600

    adding python linting rules-10

--- app-lovin/16-python-validation-script/lint_rules.py
+++ app-lovin/16-python-validation-script/lint_rules.py
@@ -1,6 +1,8 @@
 def validate_text_file(file_path):
     lines = set()
     errors = []
+    output_file_path="./output-errors.txt"
+
 
     with open(file_path, 'r') as file:
         for line_number, line in enumerate(file, start=1):  #get this: 1 152media.info, 152M10, RESELLER
@@ -15,20 +17,63 @@ def validate_text_file(file_path):
                 lines.add(line)
                             
             # Rule 2: Check for correct comma and space placement
+            #this chain would throw an error because the kk is missing a comma:
+            #152media.info kk, 152M324, RESELLER      --->error
+            #152media.info,    kk, 152M324, RESELLER  --->valid
+            #152media.info,    , 152M324, RESELLER    --->valid
+
+
+                                      #line='152media.info, 152M324, RESELLER' 
             words = line.split(', ')  #words=['152media.info', '152M324', 'RESELLER']            
 
             print([word for word in words[:-1]])
             if any(len(word.split()) > 1 for word in words[:-1]):
                 errors.append(f'Invalid comma and space placement at line {line_number}: {line}')
             
-    '''        
+            # Rule 3: Check for duplicated lines even if spacing differs        
+            # Example if there is this line:
+            #'152media.info,     152M10, RESELLER' ------> error because that line already exists even if the 
+            #                                              one that is already there has not so much spaces             
+            line_no_spaces = ''.join(line.split())  # Remove spaces by splitting and joining without spaces
+            normalized_line = ' '.join(line_no_spaces.split(','))  # Normalize spacing
+            #print(normalized_line)
+            error=False
+            for myline in lines:
+                normalized_dest = ' '.join(myline.split(', '))  # Normalize spacing
+                if normalized_line == normalized_dest:
+                    errors.append(f'Duplicate line found at line {line_number}: {line}')
+                    error=True
+                    break
+                else:                    
+                    error=False                    
+            if not error:
+                lines.add(line)
+
+            # Rule 4: Check for the count of commas and words in a row, this rule check if there are more commas than words
+            # example line='152media.info,,152M10,RESELLER'
+            comma_count = line.count(',')
+            word_count = len(words)
+            print(words)
+            print(word_count)
+            if comma_count >= word_count:
+                errors.append(f'Error at line  {line_number}: Comma count ({comma_count}) is equal to or greater than word count ({word_count}): {line}')
+                
+
+            # Rule 5: Check for blank words
+            # example: line='152media.info, ,  ,152M10,RESELLER'
+            if any(not word.strip() for word in words):
+                errors.append(f'Error at line : Contains one or more blank words: {line}')
+                print(errors)
+
+
     if errors:
-        for error in errors:
-            print(error)
-        print(f'{len(errors)} error(s) found in the file.')
+        with open(output_file_path, 'w') as output_file:
+            for error in errors:
+                output_file.write(error + '\n')
+        print(f'{len(errors)} error(s) found in the file. Errors saved to {output_file_path}')
     else:
         print('File is valid; no errors found.')
-    '''
+    
 if __name__ == "__main__":
     file_path = "./app-ads.txt"  # Replace with the path to your text file
     validate_text_file(file_path)
