5337bbbc388b51a16d182fb58ec5d2bb33f8b601
Luis Mendez <luismendezescobar@gmail.com>
2024-03-03 18:47:23 -0600

    adding cloud armour throttling

---
+++ app-lovin/cloud-armour/00-variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {    
+  type=string
+  default = ""
+}
+
+variable "region" {
+  type=string
+  default = "us-uscentral1"
+}
+
---
+++ app-lovin/cloud-armour/03-main.tf
@@ -0,0 +1,36 @@
+module "security_policy" {
+  source = "GoogleCloudPlatform/cloud-armor/google"
+  version = "~> 2.0"
+
+  project_id                           = var.project_id
+  name                                 = "security-policy-throttle"
+  description                          = "Test Security Policy"
+  #recaptcha_redirect_site_key          = google_recaptcha_enterprise_key.primary.name
+  default_rule_action                  = "allow"
+  type                                 = "CLOUD_ARMOR"
+  layer_7_ddos_defense_enable          = false
+  layer_7_ddos_defense_rule_visibility = "STANDARD"
+  
+  # Action against specific IP addresses or IP adress ranges
+  security_rules = {
+
+    "throttle_project" = {
+      action        = "throttle"
+      priority      = 1100
+      description   = "Throttle "
+      #src_ip_ranges = [""]
+
+      rate_limit_options = {
+        exceed_action                        = "deny(502)"
+        rate_limit_http_request_count        = 10
+        rate_limit_http_request_interval_sec = 60
+        enforce_on_key_configs = [
+          {
+            enforce_on_key_type = "ALL"
+          },          
+        ]
+      }
+    }
+  }
+    
+}
